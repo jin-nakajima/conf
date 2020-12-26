170fd35704a2e7b3c5a9b465243e625aac6269af
jin-nakajima <jinius.nakajima180405@gmail.com>
2020-12-27 01:26:43 +0900

    fish conf

---
+++ config.fish
@@ -0,0 +1,7 @@
+set -g theme_newline_cursor yes
+set -g theme_display_git_master_branch yes
+set -g theme_color_scheme solarized
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
